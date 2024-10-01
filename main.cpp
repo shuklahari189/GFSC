6296e687bc8e12ae43512972ba55cd9f224c28b1
hari <shuklahari189@gmail.com>
2024-10-01 19:54:57 +0530

    Day 003 done!

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <windows.h>
+
 #define LocalPersist static
 #define GlobalVariable static
 #define Internal static
@@ -6,13 +7,66 @@
 // TODO: global for now
 GlobalVariable bool running;
 
-LRESULT Wndproc(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
+GlobalVariable BITMAPINFO bitmapInfo;
+GlobalVariable HBITMAP bitmapHandle;
+GlobalVariable void *bitmapMemory;
+GlobalVariable HDC bitmapDeviceContext;
+
+Internal void 
+win32ResizeDIBsection(int width, int height)
+{
+    if (bitmapHandle)
+    {
+        DeleteObject(bitmapHandle);
+    }
+
+    if(!bitmapDeviceContext)
+    {
+        // TODO: should we recreate under certain special circumstances ?
+        bitmapDeviceContext = CreateCompatibleDC(0);
+    }
+
+    bitmapInfo.bmiHeader.biSize = sizeof(bitmapInfo);
+    bitmapInfo.bmiHeader.biWidth = width;
+    bitmapInfo.bmiHeader.biHeight = height;
+    bitmapInfo.bmiHeader.biPlanes = 1;
+    bitmapInfo.bmiHeader.biBitCount = 32; // acutally need 24 buit for dword alignment 32
+    bitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    bitmapHandle = CreateDIBSection(
+        bitmapDeviceContext,
+        &bitmapInfo,
+        DIB_RGB_COLORS,
+        &bitmapMemory,
+        0, 0);
+}
+
+Internal void 
+win32UpdateWindow(HDC deviceContext, int x, int y, int width, int height)
+{
+    StretchDIBits(
+        deviceContext,
+        x, y, width, height,
+        x, y, width, height,
+        bitmapMemory,
+        &bitmapInfo,
+        DIB_RGB_COLORS,
+        SRCCOPY);
+}
+
+LRESULT 
+win32MainWindowCallback(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
 {
     LRESULT result = 0;
     switch (message)
     {
     case WM_SIZE:
     {
+        RECT clientRect;
+        GetClientRect(window, &clientRect);
+        int width = clientRect.right - clientRect.left;
+        int height = clientRect.bottom - clientRect.top;
+        win32ResizeDIBsection(width, height);
         OutputDebugStringA("sized\n");
     }
     break;
@@ -37,7 +91,11 @@ LRESULT Wndproc(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
     {
         PAINTSTRUCT paint;
         HDC deviceContext = BeginPaint(window, &paint);
-        
+        int x = paint.rcPaint.left;
+        int y = paint.rcPaint.top;
+        int width = paint.rcPaint.right - paint.rcPaint.left;
+        int height = paint.rcPaint.bottom - paint.rcPaint.top;
+        win32UpdateWindow(deviceContext, x, y, width, height);
         EndPaint(window, &paint);
     }
     break;
@@ -58,11 +116,12 @@ LRESULT Wndproc(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
     return result;
 }
 
-int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, PSTR commandLine, int showCode)
+int WINAPI 
+WinMain(HINSTANCE instance, HINSTANCE prevInstance, PSTR commandLine, int showCode)
 {
     WNDCLASSA windowClass = {};
     windowClass.style = CS_VREDRAW | CS_HREDRAW | CS_OWNDC;
-    windowClass.lpfnWndProc = Wndproc;
+    windowClass.lpfnWndProc = win32MainWindowCallback;
     windowClass.hInstance = instance;
     windowClass.lpszClassName = "HandmadeHeroClass";
 
